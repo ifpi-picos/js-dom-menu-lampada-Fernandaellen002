8e3022ddbfacb9c6edb49c75e6ea4e1d14b8148c
Renanlucass <renanlucas0014@gmail.com>
2024-07-15 10:30:06 -0300

    a

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@
 
 // Imagem 1
 const imagem1 = document.getElementById("lampada-01");
+const imagem2 = document.getElementById("lampada-2")
 imagem1.addEventListener("click", function () {
   console.log(imagem1.src);
   if (imagem1.src.endsWith("lampada.jpg")) {
@@ -12,3 +13,13 @@ imagem1.addEventListener("click", function () {
 });
 
 // Imagem 2
+
+document.getElementById("lampada-2").onmouseover = function() {acender()};
+
+function acender(){
+  if (imagem2.src.endsWith("lampada.jpg")) {
+    imagem2.src = "img/lampada-on.jpg";
+  } else {
+    imagem2.src = "img/lampada.jpg";
+  }
+}
